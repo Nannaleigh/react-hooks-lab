9a14e0e7c98c0ec286b09fe5e711918f475d79fc
Nannaleigh <tatianna.martinez71@gmail.com>
2022-04-01 06:54:54 -0600

    Finished app-1

--- app-1/src/Todo.js
+++ app-1/src/Todo.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Todo = *(props) => {
-    return <h2>{props.item}</h2>
-}
+const Todo = (props) => {
+  return <h2>{props.item}</h2>;
+};
 
-export default Todo
+export default Todo;
